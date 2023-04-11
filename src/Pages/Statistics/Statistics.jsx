edf4f2e5744fd1925f158f24f5ac0fa84e6a982c
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-04-11 17:35:09 +0600

    assignments statistics marks added

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,9 +1,83 @@
 import React from "react";
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  Legend,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+} from "recharts";
 
 const Statistics = () => {
+  const data = [
+    {
+      subject: "A1",
+      A: 60,
+      B: 60,
+    },
+    {
+      subject: "A2",
+      A: 60,
+      B: 58,
+    },
+    {
+      subject: "A3",
+      A: 60,
+      B: 60,
+    },
+    {
+      subject: "A4",
+      A: 60,
+      B: 60,
+    },
+    {
+      subject: "A5",
+      A: 60,
+      B: 58,
+    },
+    {
+      subject: "A6",
+      A: 60,
+      B: 60,
+    },
+    {
+      subject: "A7",
+      A: 60,
+      B: 60,
+    },
+  ];
   return (
-    <div>
-      <h1>statistics</h1>
+    <div className="hero ">
+      <div className="hero-content text-center">
+        <RadarChart
+          className="m-0"
+          cx={300}
+          cy={250}
+          outerRadius={150}
+          width={500}
+          height={500}
+          data={data}
+        >
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" />
+          <PolarRadiusAxis angle={30} domain={[0, 60]} />
+          <Radar
+            name="Get Marks"
+            dataKey="A"
+            stroke="#D21312"
+            fill="#8884d8"
+            fillOpacity={0.6}
+          />
+          <Radar
+            name="Total Marks"
+            dataKey="B"
+            stroke="#393646"
+            fill="#82ca9d"
+            fillOpacity={0.6}
+          />
+          <Legend />
+        </RadarChart>
+      </div>
     </div>
   );
 };
